8eb3d68980bc9c67b40da8479dd4eedd91843410
mightyZap <128670124+mightyZap1@users.noreply.github.com>
2024-03-28 16:32:33 +0900

    Create menuList.js

---
+++ menuList.js
@@ -0,0 +1,53 @@
+const sampleMenuList = {
+  name: "root",
+  children: [
+    {
+      name: "Actuator",
+      children: [
+        {
+          name: "17Lf Series",
+          children: []
+        },
+        {
+          name: "12Lf Series",
+          children: []
+        }
+      ]
+    },
+    {
+      name: "Software",
+      children: [
+        {
+          name: "Total Manager",
+          children: []
+        }
+      ]
+    },
+    {
+      name: "Boards",
+      children: [
+        {
+          name: "IR-CT01",
+          children: []
+        },
+        {
+          name: "IR-CT02",
+          children: []
+        }
+      ]
+    },
+    {
+      name: "Library",
+      children: [
+        {
+          name: "C++",
+          children: []
+        },
+        {
+          name: "Python",
+          children: []
+        }
+      ]
+    }
+  ]
+};
